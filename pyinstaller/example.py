3b55adf49404596294a5ddb53d04e10a5c8a6daa
Josh O'Dell <joshahdell@gmail.com>
2018-07-02 14:49:31 -0500

    test

--- pyinstaller/example.py
+++ pyinstaller/example.py
@@ -1,44 +1,30 @@
+import tkinter as tk
 from tkinter import *
-from tkinter import ttk
+
+
+def hello_world():
+    username_entry.delete(0, END)
+    username_entry.insert(0, 'Hello world!')
+    return()
+
 
 root = Tk()
 
-content = ttk.Frame(root, padding=(3,3,12,12))
-frame = ttk.Frame(content, borderwidth=5, relief="sunken", width=200, height=100)
-namelbl = ttk.Label(content, text="Name")
-name = ttk.Entry(content)
+root.resizable(False, False)
 
-onevar = BooleanVar()
-twovar = BooleanVar()
-threevar = BooleanVar()
+username = tk.Label(root, text="Username:")
+password = tk.Label(root, text="Password")
+username_entry = tk.Entry(root)
+password_entry = tk.Entry(root)
+b1 = tk.Button(root, text="Run", width=15, command=hello_world)
+b2 = tk.Button(root, text="quit", width=15, command = root.destroy)
 
-onevar.set(True)
-twovar.set(False)
-threevar.set(True)
+username.grid(column=0, row=0, padx=5, pady=5)
+username_entry.grid(column=1, row=0, padx=5, pady=5)
+password.grid(column=0, row=1, padx=5, pady=5)
+password_entry.grid(column=1, row=1, padx=5, pady=5)
+b1.grid(column=0, row=2, padx=5, pady=5)
+b2.grid(column=1, row=2, padx=5, pady=5)
 
-one = ttk.Checkbutton(content, text="One", variable=onevar, onvalue=True)
-two = ttk.Checkbutton(content, text="Two", variable=twovar, onvalue=True)
-three = ttk.Checkbutton(content, text="Three", variable=threevar, onvalue=True)
-ok = ttk.Button(content, text="Okay")
-cancel = ttk.Button(content, text="Cancel")
-
-content.grid(column=0, row=0, sticky=(N, S, E, W))
-frame.grid(column=0, row=0, columnspan=3, rowspan=2, sticky=(N, S, E, W))
-namelbl.grid(column=3, row=0, columnspan=2, sticky=(N, W), padx=5)
-name.grid(column=3, row=1, columnspan=2, sticky=(N, E, W), pady=5, padx=5)
-one.grid(column=0, row=3)
-two.grid(column=1, row=3)
-three.grid(column=2, row=3)
-ok.grid(column=3, row=3)
-cancel.grid(column=4, row=3)
-
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-content.columnconfigure(0, weight=3)
-content.columnconfigure(1, weight=3)
-content.columnconfigure(2, weight=3)
-content.columnconfigure(3, weight=1)
-content.columnconfigure(4, weight=1)
-content.rowconfigure(1, weight=1)
 
 root.mainloop()
